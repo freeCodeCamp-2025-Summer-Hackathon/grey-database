b6ddca73ce5e92ede104c103f971ee7631638b17
vinay lingam <vinaylingam18@gmail.com>
2025-07-06 20:53:09 +0530

    mongo db init setup

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
 from jobapp import create_app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 if __name__ == "__main__":
     app = create_app()
-    app.run()
+    if os.getenv("FLASK_ENV") == 'dev':
+        app.run(debug=True)
+    else:
+        app.run()
