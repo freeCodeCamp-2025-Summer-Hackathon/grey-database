0fc7122731c7d3e45244706e8bab40bc0d3acf20
Bharath Valaboju <valabojubharath@yahoo.com>
2025-07-22 13:58:08 +0530

    updated models

--- backend/jobapp/models.py
+++ backend/jobapp/models.py
@@ -1,36 +1,47 @@
 from mongoengine import (
-    Document, EmbeddedDocument,
-    StringField, ReferenceField, ListField,
-    DateTimeField, EnumField, EmbeddedDocumentField,
-    FileField, IntField
+    Document,
+    EmbeddedDocument,
+    StringField,
+    ReferenceField,
+    ListField,
+    DateTimeField,
+    EnumField,
+    EmbeddedDocumentField,
+    FileField,
+    IntField,
 )
 import enum
+
 
 class StatusEnum(enum.Enum):
     pending = "pending"
     in_progress = "in_progress"
     done = "done"
 
+
 class User(Document):
     username = StringField(required=True)
     password = StringField()
+
 
 class Note(EmbeddedDocument):
     note = StringField(required=True)
     added = DateTimeField(required=True)
     status = EnumField(StatusEnum, required=True)
 
+
+class Contact(EmbeddedDocument):
+    name = StringField(required=True)
+    email = StringField(required=True)
+
+
+class Followup(EmbeddedDocument):
+    date = DateTimeField(required=True)
+
+
 class Application(Document):
     userid = ReferenceField(User, required=True, reverse_delete_rule=2)
     name = StringField(required=True)
     notes = ListField(EmbeddedDocumentField(Note))
-
-class Contact(Document):
-    applicationid = ReferenceField(Application, required=True, reverse_delete_rule=2)
-    name = StringField(required=True)
-    email = StringField(required=True)  
-
-class Followup(Document):
-    applicationid = ReferenceField(Application, required=True, reverse_delete_rule=2)
-    date = DateTimeField(required=True)
-
+    contacts = ListField(EmbeddedDocumentField(Contact))
+    followups = ListField(EmbeddedDocumentField(Followup))
