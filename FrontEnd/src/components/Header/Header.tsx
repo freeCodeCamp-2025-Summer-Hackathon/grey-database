bdde9a0beb67304d9fda0a68f68a8398ddf91335
SidhantAnweshi <sidhantanweshi@gmail.com>
2025-08-06 21:33:07 +0530

    Remove inline styling

--- FrontEnd/src/components/Header/Header.tsx
+++ FrontEnd/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Link,useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import useAuthStore from "../../store/useAuthStore";
+import "./site.css";
 
 const Header = () => {
     const navigate = useNavigate();
@@ -15,63 +16,20 @@ const Header = () => {
 
     return (
         <>
-        <div style={{display:'flex',
-            alignItems:'center',
-            padding: '10px 20px',
-            gap:'20px',
-            borderBottom: '2px solid rgb(205,205,205)',
-            justifyContent:'space-between'
-        }}>
-        <img src='../../../public/Workwave Logo.png' style= {{height: '50px'}}/>
+        <div className="header-container" >
+        <img src='../../../public/Workwave Logo.png' className = "logo-image " />
         
-        <div style={{
-            display: 'flex',
-            gap: '20px'
-        }}>
-            <Link to="/dashboard" style={{
-                textDecoration:"none",
-                color:"#003366",
-                fontWeight:"500",
-                padding: "5px 8px",
-                borderRadius:"5px",
-            }}>Dashboard</Link>
-            <Link to="/resume" style={{
-                textDecoration:"none",
-                color:"#003366",
-                fontWeight:"500",
-                padding: "4px 6px",
-                borderRadius:"6px",
-            }}>Resumes</Link>
+        <div className= "links" >
+            <Link to="/dashboard"  className= "dashboard-link">Dashboard</Link>
+            <Link to="/resume" className= "resume-link" >Resumes</Link>
 
         </div>
         
-        <div style= {{display:"flex",alignItems:"center",gap:"10px"}}>
-            <span
-                style={{
-                    display: "inline-flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "36px",
-                    height: "36px",
-                    borderRadius: "50%",
-                    background: "#e6e6e8",
-                    fontWeight: "bold",
-                    fontSize: "16px",
-                    color: "#03040a",
-                    textTransform: "uppercase"
-                }}
-            >
+        <div className="user-section" >
+            <span className= "username">
                 {username?.[0]}
             </span>
-            <button onClick={Logout} style={{
-                backgroundColor:"#0C66E4",
-                color:"#ffffff",
-                border:"none",
-                borderRadius:"5px",
-                cursor:"pointer",
-                fontWeight:"500",
-                padding: "12px 12px"
-            }}>Logout</button>
+            <button className="logout-button"onClick={Logout} >Logout</button>
         </div>
        
          
